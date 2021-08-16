9c531e137ea668123c46b0b5f8dae3fa19c29142
howkmii <abdelhakim@univ-tiaret.dz>
2021-08-16 20:12:54 +0100

    station 20

--- SQL/weather-observation-station-20.sql
+++ SQL/weather-observation-station-20.sql
@@ -1,2 +1,2 @@
-select cast (avg(1*lat_n)as numeric(20.4))
-from (select lat_n, ra=row_number()over(order by ))
+select cast (avg(1.0 *lat_n)as numeric(20,4))
+from (select lat_n, ra=row_number()over(order by lat_n,id), rd=row_number()over(order by lat_n desc,id desc)from station)as x where ra between rd-1 and rd +1;
